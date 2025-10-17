f57797eadd0c7cb65e9bce461f431cb8456de33a
cnguyen0691 <chaunguyen0691@gmail.com>
2025-10-17 12:34:06 -0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite"
+import react from '@vitejs/plugin-react'
 import path from "node:path"
 
 // https://vite.dev/config/
 export default defineConfig({
+  plugins: [react()],
+  base: '/zoey-qa/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true
+  },
   server: {
     host: "::",
     port: 8080,
